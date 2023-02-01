d1f95526a6c13b57d419a3925030cc55dc703e9c
Jero1304 <calogeroleto13@gmail.com>
2023-02-01 16:57:06 +0100

    FEAT java variabili

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const nameEle = document.querySelector('#form-section input[name="name"]');
+const surnameEle = document.querySelector('#form-section input[name="last-name"]');
+const kmDaFareEle = document.querySelector('#form-section input[name="km-da-percorrere"]');
+const etaEle = document.querySelector('#form-section select[name="eta"]');
+const formBtnEle = document.querySelector('#form-section select[type="button"]');
+
+console.log(nameEle, surnameEle, kmDaFareEle, etaEle, formBtnEle);
+
+
+let 
